14c497b16a543e7425f329597fab31b6518675e8
unknown <hdfclocal@hdfc-m6.hdfc.net>
2020-04-08 20:38:05 +0500

    corerction to cordova pluginm time ad

--- js/script.js
+++ js/script.js
@@ -11,6 +11,23 @@ $(() => {
                 url: `http://api.aladhan.com/v1/calendar?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&method=2&month=${m}&year=${d.getFullYear()}`
             }).then((data) => {
                 console.log(data)
+                $("#prayerTime").empty();
+                for (x of data.data) {
+                    $("#prayerTime").append(
+                        `
+                       <tr>
+                       <td>${x.timings.Fajr}</td>
+                      
+                       <td>${x.timings.Dhuhr}</td>
+                       <td>${x.timings.Asr}</td>
+                       <td>${x.timings.Maghrib}</td>
+                       <td>${x.timings.Isha}</td>
+                       </tr>
+
+                        `
+                    );  
+                    
+                }
             })
 
         })
@@ -20,18 +37,31 @@ $(() => {
         $("#main").addClass("d-none");
         $("#prayer").addClass("d-none");
         $("#wudu").removeClass("d-none");
+        $("#prayerTimeDiv").addClass("d-none");
+
         return false;
     });
     $("#home").on("click", () => {
         $("#main").removeClass("d-none");
         $("#prayer").addClass("d-none");
         $("#wudu").addClass("d-none");
+        $("#prayerTimeDiv").addClass("d-none");
+
         return false;
     });
     $("#pray").on("click", () => {
         $("#main").addClass("d-none");
         $("#prayer").removeClass("d-none");
         $("#wudu").addClass("d-none");
+        $("#prayerTimeDiv").addClass("d-none");
+
+        return false;
+    });
+    $("#prayT").on("click", () => {
+        $("#main").addClass("d-none");
+        $("#prayer").addClass("d-none");
+        $("#wudu").addClass("d-none");
+        $("#prayerTimeDiv").removeClass("d-none");
         return false;
     });
 
